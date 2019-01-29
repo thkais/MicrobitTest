6d9d2a83586d76e8440a6139cd9dd81222076213
thkais <37836969+thkais@users.noreply.github.com>
2019-01-29 21:02:43 +0100

    Updates.

--- Knotech.ts
+++ Knotech.ts
@@ -1,11 +1,16 @@
-// Add your code here
+
+
 //% color="#AA278D"
 //% groups="['Knotech']"
 //% weight=100
 namespace Knotech {
     //% block
-    //% group="Knotech"
     export function Motor() {
+
+    }
+
+    //% block="bar x = $x text = $text"
+    export function bar(x: number, text: string) {
 
     }
 }
